02338c0019835c42e084843c4e2527cd22bd4d5a
santiago <santiago@LAPTOP-MGBIPMD6>
2022-11-26 20:21:40 -0500

    .

--- src/co/edu/unbosque/view/FrameTutorial.java
+++ src/co/edu/unbosque/view/FrameTutorial.java
@@ -4,10 +4,12 @@ import java.awt.BorderLayout;
 
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import co.edu.unbosque.controller.Controller;
@@ -16,10 +18,12 @@ import co.edu.unbosque.model.persistence.FileHandler;
 public class FrameTutorial extends JFrame {
 	private JPanel panel, botones;
 	private JButton atras;
+	private JLabel img;
 
 	public FrameTutorial(ActionListener listener, FileHandler handler) {
 		panel = new JPanel();
 		botones = new JPanel();
+		img = new JLabel();
 		handler.loadProperties(1);
 		atras = new JButton(handler.getProperties().getProperty("persistence.FrameTutorial.atras"));
 		botones.setLayout(new GridLayout(1, 3));
@@ -31,6 +35,9 @@ public class FrameTutorial extends JFrame {
 		this.setSize(700, 500);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
+		img.setIcon(new ImageIcon((new ImageIcon("src/co/edu/unbosque/util/images/53325ad6d5c1765a04735d8f34871f69.png")).getImage()
+				.getScaledInstance(350, 100, Image.SCALE_DEFAULT)));
+		panel.add(img);
 		this.add(panel);
 	
 	}
